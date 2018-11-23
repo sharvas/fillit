e4d005b626d2007a73417982052e8af156c6795d
Sarunas VASKELIS <svaskeli@e2r13p10.42.fr>
2018-11-23 16:22:09 +0100

    first version of the algorithm

--- algorithm.c
+++ algorithm.c
@@ -6,66 +6,61 @@
 /*   By: svaskeli <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/21 14:22:46 by svaskeli          #+#    #+#             */
-/*   Updated: 2018/11/23 14:04:43 by svaskeli         ###   ########.fr       */
+/*   Updated: 2018/11/23 16:21:29 by svaskeli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <fillit.h>
 
-/* read terminos into array[number_of_pieces][9]
- * it will store xy coordinates for 4 dots (x, y, ...) and a letter in int (A(1), B(2), ...)
+/* read terminos into array[number_of_pieces][7]
+ * it will store xy coordinates for 3 dots (excluding 0, 0) and a letter in int (1, 2, ...)
  * 
  */
 
-/*int		**ft_convert_to_coord(char piece)
-{
-	int coord[4][4];
-	int i;
-
-	if (piece == A to D)
-		coord -> xx, xx, xx, xx;
-	if (piece == D to K)
-		coord -> xx, xx, xx, xx;
-	...
-	return (coord);
-}*/
-
 int		ft_issafe(char **space, int **pieces, int x, int y)
 {
-	return (0); //cannot place;
-	return (1); //can place;
+	if (space[y][x] == '.' && space[y + *pieces[0]][x + *pieces[1]] == '.' &&
+		   space[y + *pieces[2]][x + *pieces[3]] == '.' &&
+		   space[y + *pieces[4]][x + *pieces[5]] == '.')
+		return (1);
+	else
+		return (0);
 }
 
-int		ft_update_space(char **space, int **pieces, int x, int y)
+void	ft_update_space(char **space, int **pieces, int x, int y)
 {
-	ft_remove_piece(pieces);
-	return (0); //failed to update
-	return (1); //updated
+	space[y][x] = *piece[6] + 'A';
+	space[y + *pieces[0]][x + *pieces[1]] = *pieces[6] + 'A';
+	space[y + *pieces[2]][x + *pieces[3]] = *pieces[6] + 'A';
+	space[y + *pieces[4]][x + *pieces[5]] = *pieces[6] + 'A';
 }
 
-int		*ft_remove_piece(int **pieces)
+void	ft_backtrack(char **space, int **pieces, int x, int y)
 {
-	return (0);
-	return (1);
+	space[y][x] = '.';
+	space[y + *pieces[0]][x + *pieces[1]] = '.';
+	space[y + *pieces[2]][x + *pieces[3]] = '.';
+	space[y + *pieces[4]][x + *pieces[5]] = '.';
 }
 
-int		ft_backtrack(char **space, int **pieces, int x, int y)
+/* int		ft_space_left(char **space)
 {
-	return (0); //failed to backtrack
-	return (1); //backtracked
-}
+	int y;
+	int x;
 
-int		ft_add_piece(int *pcs, int **pieces)
-{
-	return (0); //failed to add back
-	return (1); //added back
-}
-
-int		ft_space_left(char **space)
-{
+	y = 0;
+	while (space[y++] != NULL)
+	{
+		x = 0;
+		while (space[y][x++] != -1)
+		{
+			if (space[y][x] == 0)
+				//need a way to check if there is more than 3 spaces together
+		}
+	}
 	return (0); //if there are no 4 size space
 	return (1); //if there are 4 size space left
-}
+} */
 
 int		ft_recursive_solver(char **space, int **pieces)
 {
@@ -75,19 +70,19 @@ int		ft_recursive_solver(char **space, int **pieces)
 	y = 0;
 	if (*pieces == NULL)
 		return (1);
-	while (space[y++] != '\0')
+	while (space[y++] != NULL)
 	{
 		x = 0;
-		while (space[x++] != '\0')
+		while (space[y][x++] != '\0')
 		{
-			if (space[y][x] == 0 && ft_issafe(space, pieces, x, y) && ft_space_left(space))
+			if (ft_issafe(space, pieces, x, y)/* && ft_space_left(space)*/)
 			{
 				ft_update_space(space, pieces, x, y);
-				return (ft_recursive_solver(space, pieces++));
+				return (ft_recursive_solver(space, &(++*pieces)));
 			}
 			else
 			{
-				ft_backtrack(space, pieces--, x, y);
+				ft_backtrack(space, &(--*pieces), x, y);
 				return (0);
 			}
 		}
@@ -105,41 +100,38 @@ int		ft_sqrt(int num)
 	return (i);
 }
 
-int		*ft_new_istr(int size)
-{
-	int *str;
-	int	x;
-
-	x = 0;
-	if(!(str = (int*)malloc(sizeof(int) * size)))
-		return (-1);
-	while (x < size - 1)
-	{
-		str[x] = 0;
-		x++;
-	}
-	str[x] = -1; //str terminating char
-	return (str);
-}
-
-int		**ft_new_space(int min_size)
+char	**ft_new_space(int min_size)
 {
 	int y;
+	int x;
 
 	y = 0;
-	if (!(**space = (int**)malloc(sizeof(int) * min_size + 1)))
+	x = 0;
+	if (!(**space = (char**)malloc(sizeof(char) * min_size + 1)))
 		return (-1);
-	while (y < min_size)
+	while (y++ < min_size)
 	{
-		if (!(space[y] = ft_new_istr(min_size + 1)))
+		if (!(space[y] = ft_strnew(min_size)))
 			return (-1);
-		y++;
+		while (x++ < min_size)
+			space[y][x] = '.';
+		x = 0;
 	}
 	space[y] = NULL;
 	return (space);
 }
 
-char	**start_function(char **pieces)
+int		ft_listlen(int **pieces) 
+{
+	int i;
+
+	i = 0;
+	while (pieces[i] != NULL)
+		i++;
+	return (i);
+}
+
+char	**start_function(int **pieces)
 {
 	char	**space;
 	char	**tmp;
@@ -151,8 +143,13 @@ char	**start_function(char **pieces)
 	while ((res = ft_recursive_solver(space, pieces)) != 1)
 	{
 		tmp = space;
-		if (!(space = ft_new_space(min_size++)))
+		if (!(space = ft_new_space(++min_size)))
 			return (NULL);
+		while (*tmp != NULL)
+		{
+			free(*tmp);
+			*tmp++;
+		}
 		free (tmp);
 	}
 	if (res == 1)
