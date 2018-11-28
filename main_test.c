7d6922533ecea65cbf5590ae0eef2c369cbe51bf
sharvas <sarunas.vaskelis@gmail.com>
2018-11-28 09:55:13 +0100

    added the conversion function

--- main_test.c
+++ main_test.c
@@ -12,6 +12,8 @@
 
 #include "fillit.h"
 
+char	**start_function(int **pieces);
+
 void	print_array(char **tetro_array)//
 {//
 	int	i;//
@@ -23,6 +25,49 @@ void	print_array(char **tetro_array)//
 		printf("%s\n", tetro_array[i++]);//
 }//
 
+int	**ft_convert(char **array)
+{
+	int **pieces;
+	int i;
+	int n;
+	int x;
+	int y;
+	int j;
+
+	i = 0;
+	while (array[i] != NULL)
+		i++;
+	if (!(pieces = (int**)malloc(sizeof(int*) * i + 1)))
+		return (NULL);
+	while (n < i)
+	{
+		y = 0;
+		x = 0;
+		k = 0;
+		n = 0;
+		j = 0;
+		if (!(pieces[n] = (int*)malloc(sizeof(int) * 9)))
+			return (NULL);
+		while (array[n][j] != '\0')
+		{
+			if (array[n][j] == '#')
+			{
+				pieces[n][k] = y;
+				pieces[n][++k] = x;
+				k++;
+			}
+			if (array[n][j] == '\n')
+				y++;
+			j++;
+			x++;
+		}
+		pieces[n][k] = i;
+		n++;
+	}
+	pieces[n] = NULL;
+	return (pieces);
+}
+
 int		main(int argc, char **argv)
 {
 	char	*file;
@@ -30,6 +75,8 @@ int		main(int argc, char **argv)
 	int		fd;
 	char	buf[548];
 	int		read_ret;
+	char **space;
+	int **pieces;
 
 	file = NULL;
 	fd = 0;
@@ -45,8 +92,10 @@ int		main(int argc, char **argv)
 	file = ft_strndup((char *)buf, read_ret);
 	close(fd);
 	tetro_array = assign_array(file);
-	print_array(tetro_array);//
-//	algorithm(tetro_array);//
-//	free_file//
+	print_array(tetro_array);
+	pieces = ft_convert(tetro_array);
+	space = start_function(tetro_array);
+	printf_array(space);
+//	free_file
 	return (0);
 }
