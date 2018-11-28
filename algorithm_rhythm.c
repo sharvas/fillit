139d336b3755bd8b2c75dd0e3beb9fde4107c4f4
A <A@As-MacBook-Air.local>
2018-11-28 20:01:11 +0100

    cleaned comments

--- algorithm_rhythm.c
+++ algorithm_rhythm.c
@@ -21,8 +21,7 @@ int		ft_issafe(char **space, int *pieces, int x, int y)
 	if (space[y + pieces[0]][x + pieces[1]] == '.' &&
 		   space[y + pieces[2]][x + pieces[3]] == '.' &&
 		   space[y + pieces[4]][x + pieces[5]] == '.' &&
-		   space[y + pieces[6]][x + pieces[7]] == '.'/* &&
-		   ft_space_left(space)*/)
+		   space[y + pieces[6]][x + pieces[7]] == '.')
 		return (1);
 	else
 		return (0);
@@ -43,57 +42,3 @@ void	ft_backtrack(char **space, int *pieces, int x, int y)
 	space[y + pieces[4]][x + pieces[5]] = '.';
 	space[y + pieces[6]][x + pieces[7]] = '.';
 }
-
-//int		ft_space_left(char **space);
-
-/*int		ft_count(char **space, int y, int x)
-{
-	int count;
-
-	count = 0;
-	if (space[y][x] == '.')
-		count++;
-	if (space[y + 1][x] == '.')
-		count++;	
-	if (space[y][x + 1] == '.')
-		count++;	
-	if (space[y + 1][x + 1] == '.')
-		count++;	
-	if (space[y - 1][x] == '.')
-		count++;	
-	if (space[y][x - 1] == '.')
-		count++;	
-	if (space[y - 1][x - 1] == '.')
-		count++;	
-	if (space[y + 1][x - 1] == '.')
-		count++;	
-	if (space[y - 1][x + 1] == '.')
-		count++;
-	return (count);
-}*/
-
-/*int		ft_space_left(char **space)
-{
-	int y;
-	int x;
-	int count;
-	int len;
-
-	y = 1;
-	count = 0;
-	len = ft_strlen(space[y]) - 1;
-	while (y < len)
-	{
-		x = 1;
-		while (x < len)
-		{
-			if (space[y][x] == '.')
-				count = ft_count(space, y, x);
-			if (count > 3)
-				return (count);
-			x++;
-		}
-		y++;
-	}
-	return (0);
-}*/
