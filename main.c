a2747df2894614b5dc51cda0fbc954436ea4fb8f
Sarunas VASKELIS <svaskeli@e2r13p9.42.fr>
2018-12-02 13:58:34 +0100

    edits

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: dfinnis <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/26 08:39:35 by dfinnis           #+#    #+#             */
-/*   Updated: 2018/12/02 11:47:17 by svaskeli         ###   ########.fr       */
+/*   Updated: 2018/12/02 13:49:06 by svaskeli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@ int	main(int argc, char **argv)
 	int		fd;
 	char	buf[548];
 	int		ret;
+	char	*tmp;
 
 	array.sq = NULL;
 	array.tetro_array = NULL;
@@ -28,8 +29,10 @@ int	main(int argc, char **argv)
 		ft_error(array);
 	if ((ret = read(fd, buf, 547)) < 0)
 		ft_error(array);
-	array.tetro_array = ft_assign_array(ft_strndup((char *)buf, ret), array);
+	tmp = ft_strndup((char *)buf, ret);
+	array.tetro_array = ft_assign_array(tmp, array);
 	close(fd);
+	free(tmp);
 	array.pieces = ft_convert(array);
 	ft_print_array(array.tetro_array = ft_start_function(array));
 	ft_free_all(array);
